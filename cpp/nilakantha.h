83ca82e48667791a1cca23740ebb97f0e79fce7a
AmazingMech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2020-06-17 16:49:53 -0400

    Create nilakantha.h

---
+++ cpp/nilakantha.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Nilakantha {
+  class NilakanthaSeries {
+    public:
+      double p = 3;
+      void iterate();
+    private:
+      double a = 2;
+      double s = 1;
+  };
+}
